d20aca6cdcf304678fb6b6d5f30cfd2b8f428a2f
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-01-24 13:47:45 +0100

    Add project files.

---
+++ DapperASP.NET Core/Context/DapperContext.cs
@@ -0,0 +1,7 @@
+﻿namespace DapperASP.NET_Core.Context
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+    }
+}
---
+++ DapperASP.NET Core/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace DapperASP.NET_Core.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; } 
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
