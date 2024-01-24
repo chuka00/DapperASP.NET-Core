70ce6c0f47c980358a7a6eceec9208b5c8ac2dae
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-01-24 14:10:11 +0100

    added interface

---
+++ DapperASP.NET Core/Contracts/ICompanyRepository.cs
@@ -0,0 +1,9 @@
+﻿using DapperASP.NET_Core.Entities;
+
+namespace DapperASP.NET_Core.Contracts
+{
+    public interface ICompanyRepository
+    {
+        public Task<IEnumerable<Company>> GetCompanies();
+    }
+}
