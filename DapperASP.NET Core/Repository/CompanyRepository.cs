da994ff165d2d6532251a810a9b0c71fae807b0a
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-01-24 14:11:06 +0100

    Added new repository

---
+++ DapperASP.NET Core/Repository/CompanyRepository.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using DapperASP.NET_Core.Context;
+using DapperASP.NET_Core.Contracts;
+using DapperASP.NET_Core.Entities;
+
+namespace DapperASP.NET_Core.Repository
+{
+    public class CompanyRepository : ICompanyRepository
+    {
+        private readonly DapperContext _context;
+        public CompanyRepository(DapperContext context) => _context = context;
+
+        public async Task<IEnumerable<Company>> GetCompanies()
+        {
+            var query = "SELECT * FROM Companies";
+            using(var connection = _context.CreateConnection())
+            {
+                var companies = await connection.QueryAsync<Company>(query);
+                return companies.ToList();
+            }
+        }
+    }
+}
