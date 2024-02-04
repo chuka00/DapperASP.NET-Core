b902b12ad2c44dc34b3b8cbf49eb5ec0c03420e6
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-02-04 18:22:06 +0100

    Added Controller

---
+++ DapperASP.NET Core/Controllers/CompaniesController.cs
@@ -0,0 +1,21 @@
+﻿using DapperASP.NET_Core.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperASP.NET_Core.Controllers
+{
+    [Route("api/companies")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly ICompanyRepository _companyRepo;
+        public CompaniesController(ICompanyRepository companyRepo) => _companyRepo = companyRepo;
+
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies()
+        {
+            var companies = await _companyRepo.GetCompanies();
+            return Ok(companies);
+        }
+    }
+}
