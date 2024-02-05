18f9efe4862ddb9dec27276ea044822633802b54
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-02-05 12:09:06 +0100

    modified context

--- DapperASP.NET Core/Context/DapperContext.cs
+++ DapperASP.NET Core/Context/DapperContext.cs
@@ -1,7 +1,19 @@
-﻿namespace DapperASP.NET_Core.Context
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DapperASP.NET_Core.Context
 {
     public class DapperContext
     {
         private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("SqlConnection");
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
     }
 }
